b651e25de809e169517cf571f41906b3b06575d4
Georgiy <parsegovgeorgiy@gmail.com>
2018-11-28 01:37:30 +0300

    Updates

---
+++ precache-manifest.7ed7dfb436790edf01fc35ddc052e2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "297e198ed81c15217fcc",
+    "url": "/tasks/static/js/runtime~main.297e198e.js"
+  },
+  {
+    "revision": "30f03630f8d3c859d32e",
+    "url": "/tasks/static/js/main.30f03630.chunk.js"
+  },
+  {
+    "revision": "94db03d3568c4612c498",
+    "url": "/tasks/static/js/1.94db03d3.chunk.js"
+  },
+  {
+    "revision": "30f03630f8d3c859d32e",
+    "url": "/tasks/static/css/main.ef2ab9df.chunk.css"
+  },
+  {
+    "revision": "94db03d3568c4612c498",
+    "url": "/tasks/static/css/1.dc38e3c4.chunk.css"
+  },
+  {
+    "revision": "702800e8880aee463485522deb661be6",
+    "url": "/tasks/index.html"
+  }
+];
